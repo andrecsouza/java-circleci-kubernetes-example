2784eeafc53bfb3394d44d48e34bcd56868a3ae7
Andre C de Souza <andre.cardoso.souza@gmail.com>
2019-07-16 17:34:48 -0300

    Cria aplicacao Spring Boot com endpoint de saude

---
+++ src/main/java/org/andrecs/examples/javacicd/JavaCicdApplication.java
@@ -0,0 +1,13 @@
+package org.andrecs.examples.javacicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCicdApplication.class, args);
+	}
+
+}
